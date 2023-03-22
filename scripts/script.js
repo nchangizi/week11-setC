0ebe2790aec4199b1f57e003783c2bb41b88802b
Neda <nedachangizi@gmail.com>
2023-03-22 13:42:39 -0700

    update paragraphs based on circle

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
-function greet(name, time = "day")
-{
-    console.log(`Good ${time} ${name}`);
-}
+// function greet(name, time = "day")
+// {
+//     console.log(`Good ${time} ${name}`);
+// }
 
 // // let input = prompt("what is your name?");
 // // greet(input);
-greet("Neda");
-greet("Neda", "evening", "sandwich");
+// greet("Neda");
+// greet("Neda", "evening", "sandwich");
 
 // const greet = function (name)
 // {
@@ -20,3 +20,37 @@ greet("Neda", "evening", "sandwich");
 // }
 
 // greet("Neda");
+
+const radiusPara = document.querySelector("#radius");
+console.log(radiusPara);
+const resultPara = document.querySelector("#result");
+console.log(resultPara);
+
+function calculateCircleArea(radius) {
+  if (isNaN(radius)) {
+    // replace this alert by updating the text of resultPara
+    resultPara.textContent = "This is not a number";
+    // alert("This is not a number");
+    return;
+    // returns undefined by default
+  } else {
+    const area = Math.PI * radius ** 2;
+    return area;
+  }
+}
+
+let userValue = prompt("Enter a number");
+// update the text of the radiuspara
+radiusPara.textContent += userValue;
+// prompt always returns a string
+let result = calculateCircleArea(userValue);
+if (result !== undefined) {
+  // replace alert with updating text of resultPara
+  resultPara.textContent = `The area of a circle with radius ${userValue} is ${result.toFixed(
+    2
+  )}`;
+
+  //   alert(
+  //     `The area of a circle with radius ${userValue} is ${result.toFixed(2)}`
+  //   );
+}
